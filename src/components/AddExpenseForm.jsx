839f362f76cb4dd91c15ad5ca0fcc15b11992ce9
chirag4862 <chiragrvijay@gmail.com>
2023-04-25 22:14:09 +0530

    Base Expense Form UI

---
+++ src/components/AddExpenseForm.jsx
@@ -0,0 +1,63 @@
+import { useRef } from "react";
+// rrd imports
+import { useFetcher } from "react-router-dom";
+
+const AddExpenseForm = ({ budgets }) => {
+  const fetcher = useFetcher();
+  const formRef = useRef();
+  const focusRef = useRef();
+  return (
+    <div className="form-wrapper">
+      <h2 className="h3">
+        Add New{" "}
+        <span className="accent">
+          {budgets.length === 1 && `${budgets.map((budg) => budg.name)}`}
+        </span>{" "}
+        Expense
+      </h2>
+      <fetcher.Form method="post" className="grid-sm" ref={formRef}>
+        <div className="expense-inputs">
+          <div className="grid-xs">
+            <label htmlFor="newExpense">Expense Name</label>
+            <input
+              type="text"
+              name="newExpense"
+              id="newExpense"
+              required
+              placeholder="e.g., Taxi"
+              ref={focusRef}
+            />
+          </div>
+          <div className="grid-xs">
+            <label htmlFor="newExpenseAmount">Amount</label>
+            <input
+              type="number"
+              step="0.01"
+              inputMode="decimal"
+              name="newExpenseAmount"
+              id="newExpenseAmount"
+              required
+              placeholder="e.g., 50"
+            />
+          </div>
+        </div>
+        <div className="grid-xs">
+          <label htmlFor="newExpenseBudget">Budget Category</label>
+          <select name="newExpenseBudget" id="newExpenseBudget" required>
+            {budgets
+              .sort((a, b) => a.createdAt - b.createdAt)
+              .map((budget) => {
+                return (
+                  <option value={budget.id} key={budget.id}>
+                    {budget.name}
+                  </option>
+                );
+              })}
+          </select>
+        </div>
+      </fetcher.Form>
+    </div>
+  );
+};
+
+export default AddExpenseForm;
